449d7cdcac589dd478c4341758848929aacdb951
hoshoo21 <c_gawera@yahoo.com>
2025-11-20 10:56:46 -0800

    cocktails implementation

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { useGSAP } from '@gsap/react'
 import { ScrollTrigger, SplitText } from 'gsap/all'
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Cocktails from './components/Cocktails';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 function App() {
@@ -12,7 +13,7 @@ function App() {
    <main>
     <Navbar />
     <Hero />
-    <div className='h-dvh bg-black'></div>
+    <Cocktails />
    </main>
   )
 }
