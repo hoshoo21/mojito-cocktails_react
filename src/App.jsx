fe6c76272c446dbcaa95fe34a8adce8d67f08132
hoshoo21 <c_gawera@yahoo.com>
2025-11-20 07:03:42 -0800

    hero implemention

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import gsap from 'gsap'
 import { useGSAP } from '@gsap/react'
 import { ScrollTrigger, SplitText } from 'gsap/all'
 import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 function App() {
@@ -10,6 +11,8 @@ function App() {
   return (
    <main>
     <Navbar />
+    <Hero />
+    <div className='h-dvh bg-black'></div>
    </main>
   )
 }
